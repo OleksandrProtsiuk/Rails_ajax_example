0516e1109de13a20fe91ff3e7f8d40a9869d7703
alex <alex_is_ok@yahoo.com>
2019-01-11 09:41:02 +0200

    commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+An example of ajax usage in rails.
+Visit <a href="https://rubyplus.com/articles/4211-Using-Ajax-and-jQuery-in-Rails-5-Apps">this</a> page for more details.
+* Ruby version  2.5.1
+* Rails version  5.2.2
---
+++ app/assets/javascripts/tasks.js
@@ -0,0 +1,18 @@
+//= require rails-ujs
+//= require activestorage
+//= require jquery
+// require turbolinks
+//= require_tree .
+
+
+jQuery.fn.submitOnCheck = function() {
+    this.find('input[type=submit]').remove();
+    this.find('input[type=checkbox]').click(function() {
+        $(this).parent('form').submit();
+    });
+    return this;
+};
+
+$(function() {
+    $('.edit_task').submitOnCheck();
+});
